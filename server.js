f2219e914bf982589208f98cc992d2d0d7c14832
H-Richard <r.hong@protonmail.com>
2019-02-15 13:33:21 -0500

    username

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:0a6c33db-1285-4a0a-a2d0-bd2d9365c3c9',
+  key: '826fc087-f0cf-426e-ab73-7fcb1243745b:Za2Yaz5FbWX77Z1u0WJ0ANrWE3o9+HnCV9STkQP/aJc=',
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+
+  chatkit
+    .createUser({
+      name: username,
+      id: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists'){
+        res.sendStatus(200)
+      }
+      else {
+        res.status(error.statusCode).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
+
 
 const PORT = 3001
 app.listen(PORT, err => {
